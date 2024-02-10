38a51cbcc10c9a542731c29f756c9ec699dd1219
danieldev402 <asencio.danielfelipe@gmail.com>
2024-02-10 10:00:17 -0500

    feat: :sparkles: calculate sets
    all the functions will be here

---
+++ calculator.py
@@ -0,0 +1,21 @@
+from main import *
+
+def union():
+
+    print ( "union")
+
+def intersection():
+
+    print ("intersection")
+
+def difference():
+
+    print ("difference")
+
+def simetric_difference():
+
+    print ("simetric_difference")
+
+def complementary_difference():
+    
+    print ("complementary_difference")
